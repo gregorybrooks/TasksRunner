ef54cbe23a9db14f1d28437a6de354a8a4c55a57
Greg Brooks <gregorybrooks@gmail.com>
2021-12-11 14:26:44 -0500

    A test version

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -1,5 +1,5 @@
 set -v
 DOCKERHUB_USER=gregorybrooks
-docker rmi $DOCKERHUB_USER/tasks-runner:3.1.0
-docker build -t $DOCKERHUB_USER/tasks-runner:3.1.0 .
-docker push gregorybrooks/tasks-runner:3.1.0
+docker rmi $DOCKERHUB_USER/tasks-runner:3.1.0-TEST
+docker build -t $DOCKERHUB_USER/tasks-runner:3.1.0-TEST .
+docker push gregorybrooks/tasks-runner:3.1.0-TEST
--- scripts/run_ir.sh
+++ scripts/run_ir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ve
 
-cd target && java -jar tasks-runner-3.1.0.jar
+cd target && java -jar tasks-runner-3.1.0-TEST.jar
 
 chmod a+rw ${appFileLocation}/results.json
